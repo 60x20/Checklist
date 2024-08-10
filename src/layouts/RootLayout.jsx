326e175825eab04f4e88079982325afbf95c3d7e
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-11 01:29:47 +0300

    validation used in root layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -10,7 +10,14 @@ import Footer from "../components/Footer";
 import MenuStateProvider from "../providers/MenuStateProvider";
 import CurrentDateProvider from "../providers/CurrentDateProvider";
 
+// helpers
+import { validateTodosTemplate } from "../helpers/todosTemplateHelpers";
+import { validateAllTodos } from "../helpers/allTodosHelpers";
+
 const RootLayout = () => {
+  // validation for localStorage entries of todosTemplate and allTodos; they must exist
+  validateTodosTemplate();
+  validateAllTodos();
 
   return (<>
     <MenuStateProvider>
