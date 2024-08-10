2706a60bc01b978e3b6d93c007f806a4101f492f
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-11 01:28:38 +0300

    all todos helpers added

---
+++ src/helpers/allTodosHelpers.js
@@ -0,0 +1,30 @@
+// All ToDos: binds todo string to unique ids, used to access todo strings
+
+function setAllTodos(arrayOfTodoStrings) {
+  // array indexes are used as references to ToDos, so indexes MUST NOT change
+  localStorage.setItem('todos', JSON.stringify(arrayOfTodoStrings));
+}
+
+export function validateAllTodos() {
+  if (!localStorage.getItem('todos')) {
+    setAllTodos([]);
+  }
+}
+
+export function returnAllTodos() {
+  return JSON.parse(localStorage.getItem('todos'));
+}
+
+export function addToAllTodos(todoString) {
+  const allTodos = returnAllTodos();
+  allTodos.push(todoString);
+  setAllTodos(allTodos);
+  // returns the ID assigned to todoString
+  return allTodos.length - 1;
+}
+
+export function updateTodoString(id, todoString) {
+  const allTodos = returnAllTodos();
+  allTodos[id] = todoString;
+  setAllTodos(allTodos);
+}
