c38ada790abe9c25089f0729fa61d33c000359af
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-11 01:27:40 +0300

    todos template helpers added

---
+++ src/helpers/todosTemplateHelpers.js
@@ -0,0 +1,27 @@
+// todosTemplate: todos that used lastly, used as a template for vacant dates
+
+function setTodosTemplate(arrayOfIds) {
+  localStorage.setItem('todos-template', JSON.stringify(arrayOfIds));
+}
+
+export function validateTodosTemplate() {
+  if (!localStorage.getItem('todos-template')) {
+    setTodosTemplate([]);
+  }
+}
+
+export function returnTodosTemplate() {
+  return JSON.parse(localStorage.getItem('todos-template'));
+}
+
+export function addToTodosTemplate(id) {
+  const todosTemplate = returnTodosTemplate();
+  todosTemplate.push(id);
+  setTodosTemplate(todosTemplate);
+}
+
+export function removeFromTodosTemplate(idToRemove) {
+  const todosTemplate = returnTodosTemplate();
+  const filteredTodosTemplate = todosTemplate.filter((id) => id !== idToRemove);
+  setTodosTemplate(filteredTodosTemplate);
+}
