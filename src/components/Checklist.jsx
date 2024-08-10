f252064142089edbb762492a5d84636565b349b3
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-11 01:31:31 +0300

    when date changes bring the new data

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 // contexts
@@ -16,6 +17,14 @@ const Checklist = () => {
   const { year, month, day } = requestedDate;
 
   const currentDate = useContext(currentDateContext);
+  const [currentToDoData, setCurrentToDoData] = useState({});
+  
+  useEffect(() => {
+    // values must be num, so that they are considered array indexes
+    const unitsAsInt = [parseInt(year, 10), parseInt(month, 10), parseInt(day, 10)];
+    validateToDoData(...unitsAsInt);
+    setCurrentToDoData(returnTodoData(...unitsAsInt));
+  }, [day, month, year]);
   
   return (
     <div id="checklist">
