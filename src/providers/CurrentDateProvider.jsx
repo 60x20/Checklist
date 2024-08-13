b309b093a88aef8e024e6db3c2b677bb7a67a338
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-14 01:26:31 +0300

    interval timing

--- src/providers/CurrentDateProvider.jsx
+++ src/providers/CurrentDateProvider.jsx
@@ -24,7 +24,7 @@ const CurrentDateProvider = ({ children }) => {
   useEffect(
     () => {
       // since currentDate gets changed, if we don't re-set the interval with the new refreshCurrentDate, we'll be using the older version
-      const intervalID = setInterval(refreshCurrentDate, 5000);
+      const intervalID = setInterval(refreshCurrentDate, 30e3);
       // clean-up: clear the previous refresher, so that there's 1 refresher at a time
       return clearInterval.bind(globalThis, intervalID);
     }, [currentDate]
