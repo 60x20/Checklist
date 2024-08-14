fb2d8e6f3698373fb9722bfc9b85c36a2ea2e9ec
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-15 01:25:12 +0300

    date getters added

--- src/helpers/returnCurrentDate.js
+++ src/helpers/returnCurrentDate.js
@@ -1,13 +1,23 @@
-export default function returnCurrentDate() {
-  const currentDate = new Date();
-  
-  const year = currentDate.getFullYear();
-  // month is zero-indexed
-  const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-  const day = String(currentDate.getDate()).padStart(2, '0');
+const dayInMs = 1000 * 60 * 60 * 24;
+
+function returnDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // month is zero-indexed
+  const day = String(date.getDate()).padStart(2, '0');
   
   return ({
     DMY: [day, month, year].join('-'),
     YMD: [year, month, day].join('-')
   });
+}
+
+export function returnCurrentDate() {
+  const currentDate = new Date();
+  return returnDate(currentDate);
+}
+
+export function returnDateFromToday(day) {
+  const currentDate = Date.now();
+  const relativeDate = new Date(currentDate + day * dayInMs);
+  return returnDate(relativeDate);
 }
