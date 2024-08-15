ace25ac972b26c1096a6f44f4d8594a12887b42b
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-16 00:34:09 +0300

    all years validation after cleared

--- src/helpers/resetAllData.js
+++ src/helpers/resetAllData.js
@@ -1,8 +1,10 @@
 import { validateTodosTemplate } from "../helpers/todosTemplateHelpers";
 import { validateAllTodos } from "../helpers/allTodosHelpers";
+import { validateAllYears } from "./allYearsHelpers";
 
 export default function resetAllData() {
   localStorage.clear();
   validateTodosTemplate();
   validateAllTodos();
+  validateAllYears();
 }
