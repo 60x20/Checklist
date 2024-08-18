d0fe404bab7edfd74754c921eb654188ca9a3ae3
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-18 19:47:53 +0300

    month visualizer added and helpers imported

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,8 +1,43 @@
 import { Link, Outlet, useParams } from "react-router-dom";
+
+// helpers
+import { extractYear, extractMonth, validateDate } from "../helpers/validateUnitsFromDate";
+import { returnYearEntry } from "../helpers/todoDataHelpers";
 export const VisualizerLayout = () => {
   return (
     <div id="visualizer">
       <Outlet />
     </div>
   );
+}
+
+export const MonthVisualizer = () => {
+  // a specific month requested
+  const { year, month } = useParams();
+
+  const extractedYear = extractYear(year);
+  const extractedMonth = extractMonth(month);
+
+  const isValid = validateDate(extractedYear, extractedMonth);
+  if (!isValid) return <p>invalid date</p>;
+
+  const yearAsInt = parseInt(extractedYear, 10);
+  const monthAsInt = parseInt(extractedMonth, 10);
+
+  const yearEntry = returnYearEntry(yearAsInt);
+  if (!yearEntry) return (<p>no data for year</p>);
+
+  const monthEntry = yearEntry[monthAsInt];
+  if (!monthEntry) return (<p>no data for month</p>);
+
+  return (<>
+    { monthEntry.map((dayData, day) => dayData ? (
+      // there are vacant indexes, so that days and indexes match
+      <div key={day} className="day">
+        { Object.entries(dayData).map(([todoId, checked]) => (
+          <p key={todoId}>{checked ? 'finished' : 'unfinished'}</p>
+        )) }
+      </div>
+    ) : false) }
+  </>);
 }
