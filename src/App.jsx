aa25d91f97ef2aff20710c78ced306236d450667
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-18 19:50:00 +0300

    visualizers placed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import RootLayout from './layouts/RootLayout';
 
 // components
 import Checklist from './components/Checklist';
+import { VisualizerLayout, AllYearsVisualizer, YearVisualizer, MonthVisualizer } from './components/Visualizer';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
+      <Route path="all" element={<VisualizerLayout />}>
+        <Route index element={<AllYearsVisualizer />} />
+        <Route path=":year" element={<YearVisualizer />} />
+        <Route path=":year/:month" element={<MonthVisualizer />} />
+      </Route>
       <Route path=":year/:month/:day" element={<Checklist />} />
       <Route path="*" />
     </Route>
