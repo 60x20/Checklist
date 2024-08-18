6325f354ec3f23c5907caaf7759f27283832ea53
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-18 19:49:32 +0300

    spacing

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -4,7 +4,9 @@ import { Link, Outlet, useParams } from "react-router-dom";
 import { returnAllYears } from "../helpers/allYearsHelpers";
 import { extractYear, extractMonth, validateDate } from "../helpers/validateUnitsFromDate";
 import { returnYearEntry } from "../helpers/todoDataHelpers";
+
 const monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
 export const VisualizerLayout = () => {
   return (
     <div id="visualizer">
