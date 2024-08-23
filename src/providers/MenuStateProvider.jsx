97c2189c0e24751aa3f2080872703ca23622dd5f
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-24 00:19:34 +0300

    when escape pressed, close the menu

--- src/providers/MenuStateProvider.jsx
+++ src/providers/MenuStateProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const menuStateContext = createContext();
 
@@ -7,6 +7,18 @@ const MenuStateProvider = ({ children }) => {
   function toggleMenuState() {
     setMenuState(!menuState);
   }
+  function closeTheMenu() {
+    setMenuState(false);
+  }
+
+  // close the menu when escape pressed (for accessibility)
+  useEffect(() => {
+    const closeMenuHandler = (e) => {
+      if (e.key === 'Escape') closeTheMenu();
+    };
+    document.addEventListener('keyup', closeMenuHandler);
+    return () => document.removeEventListener('keyup', closeMenuHandler);
+  }, []);
 
   return ( 
     <menuStateContext.Provider value={{ menuState, toggleMenuState }}>
