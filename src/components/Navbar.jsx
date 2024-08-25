0e174c6664f4d97db6da02b5b59937e0d3022d43
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-26 01:03:30 +0300

    navbar uses theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 
 // context providers
 import { menuStateContext } from '../providers/MenuStateProvider';
+import { themeContext } from '../providers/ThemeProvider';
 
 // font awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -9,7 +10,8 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   const { menuState, toggleMenuState } = useContext(menuStateContext);
-  
+  const { preferenceForDark, togglePreferenceForDark } = useContext(themeContext);
+
   return (
     <nav id="top-navigation-bar" className="text-and-group-row-container">
       <h2>Checklist</h2>
