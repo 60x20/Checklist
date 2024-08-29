5683c8edc6190d48e59dd16a88a31cbd428ffbcb
60x20 <120422100+60x20@users.noreply.github.com>
2024-08-30 01:09:13 +0300

    todayCleared provider placed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,17 @@
 // providers
 import AmountOfClearsProvider from "../providers/AmountOfClearsProvider";
+import TodayClearedProvider from "../providers/TodayClearedProvider";
 import RequestedDateValidatedProvider from "../providers/RequestedDateValidatedProvider";
 
 const Main = ({ children }) => {
   return (
     <main>
       <AmountOfClearsProvider>
+        <TodayClearedProvider>
           <RequestedDateValidatedProvider>
             {children}
           </RequestedDateValidatedProvider>
+        </TodayClearedProvider>
       </AmountOfClearsProvider>
     </main>
   );
