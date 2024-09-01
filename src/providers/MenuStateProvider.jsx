11d80adaaf0255d322907cb896bd9a8b80c0d5d2
60x20 <120422100+60x20@users.noreply.github.com>
2024-09-02 02:04:38 +0300

    close the menu momoized because will be used as a dependency

--- src/providers/MenuStateProvider.jsx
+++ src/providers/MenuStateProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 export const menuStateContext = createContext();
 
@@ -7,9 +7,10 @@ const MenuStateProvider = ({ children }) => {
   function toggleMenuState() {
     setMenuState(!menuState);
   }
-  function closeTheMenu() {
+  // memoized because used as a dependency
+  const closeTheMenu = useCallback(() => {
     setMenuState(false);
-  }
+  }, [setMenuState]);
 
   // close the menu when escape pressed (for accessibility)
   useEffect(() => {
