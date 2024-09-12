53de2664bf10fa0703c7f3dbebf548ebee3a344f
60x20 <120422100+60x20@users.noreply.github.com>
2024-09-12 22:53:37 +0300

    react import removed

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root'))
   .render(
-    <React.StrictMode>
+    <StrictMode>
       <App />
-    </React.StrictMode>,
+    </StrictMode>,
   )
 ;
