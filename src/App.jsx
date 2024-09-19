fa4977817bc24a2a0088f66f1b303cd05a3dfdfd
60x20 <120422100+60x20@users.noreply.github.com>
2024-09-19 22:54:59 +0300

    spacing

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,18 @@ import Home from './components/Home';
 import { VisualizerLayout, AllYearsVisualizer, YearVisualizer, MonthVisualizer } from './components/Visualizer';
 
 // TODO: don't use hash router
-const router = createHashRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="all" element={<VisualizerLayout />}>
-        <Route index element={<AllYearsVisualizer />} />
-        <Route path=":year" element={<YearVisualizer />} />
-        <Route path=":year/:month" element={<MonthVisualizer />} />
-      </Route>
-      <Route path=":year/:month/:day" element={<Checklist />} />
-      <Route path="*" element={<Home />} />
+const router = createHashRouter(createRoutesFromElements(
+  <Route path="/" element={<RootLayout />}>
+    <Route index element={<Home />} />
+    <Route path="all" element={<VisualizerLayout />}>
+      <Route index element={<AllYearsVisualizer />} />
+      <Route path=":year" element={<YearVisualizer />} />
+      <Route path=":year/:month" element={<MonthVisualizer />} />
     </Route>
-  )
-);
+    <Route path=":year/:month/:day" element={<Checklist />} />
+    <Route path="*" element={<Home />} />
+  </Route>
+));
 
 function App() {
   return (
