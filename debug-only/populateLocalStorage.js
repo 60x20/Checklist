d8ddfc48696a80fdf11ff6e35adc9b8473e92ae4
60x20 <120422100+60x20@users.noreply.github.com>
2024-09-29 17:37:38 +0300

    populateAllTodos added for testing

--- debug-only/populateLocalStorage.js
+++ debug-only/populateLocalStorage.js
@@ -1,9 +1,19 @@
 // populating for easier performance auditing
-function populateLocalStorage(population = 1000, year = 2024, month = 1, day = 1) {
-  const yearEntry = JSON.parse(localStorage[year]);
+
+function populateDate(population = 1000, year = 2024, month = 1, day = 1) {
+  const yearEntry = JSON.parse(localStorage.getItem(year));
   const newDayData = [];
   newDayData.length = population;
   newDayData.fill(0);
   yearEntry[month][day] = ({...newDayData});
   localStorage.setItem(year, JSON.stringify(yearEntry));
+}
+
+function populateAllTodos(population = 1000, text = 'text') {
+  const todosEntry = JSON.parse(localStorage.getItem('todos'));
+  const newTodos = [];
+  newTodos.length = population;
+  newTodos.fill(text);
+  const populatedTodos = todosEntry.concat(newTodos);
+  localStorage.setItem('todos', JSON.stringify(populatedTodos));
 }
