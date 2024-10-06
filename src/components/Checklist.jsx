0d9a593db75e2f1b87c6e5e25707b7ad6e452246
60x20 <120422100+60x20@users.noreply.github.com>
2024-10-06 21:40:50 +0300

    props passed to TodoState

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -223,7 +223,7 @@ const Todo = memo(({ updateCurrentTodoData, day, month, year, unitsAsInt, todoId
   return (<li className="column-container todo" ref={todoRef}>
     <div className="main-with-others-grouped-row-container">
       <h3 className="main-item styled-as-p">{todoDescription}</h3>
-      <TodoState />
+      <TodoState { ...{todoId, todoValue, todoType, updateTodoCheckedHandler} } />
       <button
         className="toggler-with-icon helper-menu-toggler"
         onClick={() => toggleHelperState()}
@@ -241,9 +241,9 @@ const Todo = memo(({ updateCurrentTodoData, day, month, year, unitsAsInt, todoId
   </li>);
 });
 
-const TodoState = () => {
   return (<input name="todo-state" type="checkbox" data-id-to-update={todoId} onChange={updateTodoCheckedHandler} checked={todoValue}
     title={`Mark as ${!todoValue ? 'done' : 'undone'}.`}
+const TodoState = ({ todoId, todoValue, todoType, updateTodoCheckedHandler }) => {
   />);
 }
 
