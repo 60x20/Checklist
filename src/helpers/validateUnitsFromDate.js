e7507355ca005e6341192ce5453a82ff17586d89
60x20 <120422100+60x20@users.noreply.github.com>
2024-10-13 20:30:15 +0300

    dayNames added

--- src/helpers/validateUnitsFromDate.js
+++ src/helpers/validateUnitsFromDate.js
@@ -1,3 +1,4 @@
+export const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 export const monthNames = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 export const monthNamesTruncated = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
