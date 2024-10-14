096bc9d4867ed2289288f5376bdd5638c3eafd0e
60x20 <120422100+60x20@users.noreply.github.com>
2024-10-14 21:51:17 +0300

    text-and-icon styling extracted into class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,6 +35,7 @@ const Header = () => {
       </button>
       <button
         type="button"
+        className="toggler-text-and-icon"
         id="theme-toggler"
         onClick={() => toggleThemeMode()}
         title={`Switch to ${themeModeData[returnThemeMode(themeMode + 1)].asWord} theme.`}
