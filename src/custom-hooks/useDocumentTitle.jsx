836949813c81cbbe7d0641d94a6135f6e897e0f5
60x20 <120422100+60x20@users.noreply.github.com>
2024-10-30 21:08:35 +0300

    exhaustive-deps for useDocumentTitle

--- src/custom-hooks/useDocumentTitle.jsx
+++ src/custom-hooks/useDocumentTitle.jsx
@@ -6,7 +6,7 @@ export default function useDocumentTitle(mainTitle = originalTitle, subtitle = '
   // layout effect preferred to decrease the delay
   useLayoutEffect(() => { // when mounts or subtitle changes, use the titles
     document.title = subtitle ? [mainTitle, subtitle].join(' - ') : mainTitle;
-  }, [subtitle]);
+  }, [mainTitle, subtitle]);
   useLayoutEffect(() => { // when unmounts, revert back to the original
     return () => document.title = originalTitle;
   }, []);
