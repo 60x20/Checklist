f33c12554bf85081f5e0a84eb9d6c364ea35be27
60x20 <120422100+60x20@users.noreply.github.com>
2024-11-28 00:27:20 +0300

    utils.ts adopts ts

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,11 +1,11 @@
-export function capitalizeString(str) {
+export function capitalizeString(str: string) {
   return str[0].toUpperCase() + str.substring(1);
 }
 
-export function truncateString(str, maxCharSize = 10) {
+export function truncateString(str: string, maxCharSize = 10) {
   return str.length > maxCharSize ? str.substring(0, maxCharSize) + '...' : str;
 }
 
-export function isArrTruthy(arr) {
+export function isArrTruthy(arr: unknown[]) {
   return arr.some(Boolean);
 }
