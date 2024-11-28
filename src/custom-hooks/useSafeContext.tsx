fee975649b871f7def85f8d0f089646c38974f5d
60x20 <120422100+60x20@users.noreply.github.com>
2024-11-28 18:21:15 +0300

    Create useSafeContext.tsx

---
+++ src/custom-hooks/useSafeContext.tsx
@@ -0,0 +1,7 @@
+import { useContext } from 'react';
+
+export default function useSafeContext<ContextValueType>(context: React.Context<ContextValueType>) {
+  const contextValue = useContext(context);
+  if (contextValue) return contextValue;
+  throw new Error('context value is null');
+}
