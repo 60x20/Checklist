760de8c622f03b8842805e099c64580b889c2703
60x20 <120422100+60x20@users.noreply.github.com>
2024-11-28 18:38:22 +0300

    themeHelpers adopts ts

--- src/helpers/themeHelpers.ts
+++ src/helpers/themeHelpers.ts
@@ -1,5 +1,5 @@
 // font awesome
-import { faSun, faMoon, faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faCircleHalfStroke, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 export const colorSchemeMediaQuery = matchMedia('(prefers-color-scheme: dark)');
 
@@ -7,26 +7,39 @@ export function isDarkPreferred() {
   return colorSchemeMediaQuery.matches;
 }
 
+export type ThemeMode = 0 | 1 | 2;
+
 export function returnThemeEntry() {
-  return JSON.parse(localStorage.getItem('theme'));
+  const themeEntry = localStorage.getItem('theme');
+  if (themeEntry !== null) return JSON.parse(themeEntry) as ThemeMode;
+  return null;
 }
 
-export function changeThemeModeEntry(value) {
-  localStorage.setItem('theme', JSON.stringify(value));
+export function changeThemeModeEntry(themeMode: ThemeMode) {
+  localStorage.setItem('theme', JSON.stringify(themeMode));
 }
 
-export function returnThemeMode(mode) {
-  switch (mode) {
+export function returnThemeMode(themeMode: ThemeMode | number | null) {
+  switch (themeMode) {
     case 1:
       return 1; // light
     case 2:
       return 2; // dark
     default:
-      return 0; // auto, also a fallback (if it's undefined, or bigger than 2)
+      return 0; // auto, also a fallback (if it's null, or bigger than 2)
   }
 }
 
-export const themeModeData = {
+type ThemeModeData = {
+  [Prop in ThemeMode]: ThemeModeDatum;
+};
+interface ThemeModeDatum {
+  asWord: string;
+  icon: IconDefinition;
+  preferenceForDark: boolean;
+}
+
+export const themeModeData: ThemeModeData = {
   0: {
     asWord: 'auto',
     icon: faCircleHalfStroke,
