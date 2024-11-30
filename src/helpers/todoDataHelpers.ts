9caaba097a198d7706f28fe7d8c8d51b302f97bb
60x20 <120422100+60x20@users.noreply.github.com>
2024-11-30 18:56:39 +0300

    todoDataHelpers adopts ts

--- src/helpers/todoDataHelpers.ts
+++ src/helpers/todoDataHelpers.ts
@@ -1,25 +1,49 @@
 // TODO: IndexedDB might be used instead of LocalStorage, due to how structured the data is
 // and this might help with localization of state
 
-import { returnTodosTemplateForWeekday } from './todosTemplateHelpers';
+import { returnTodosTemplateForWeekday, Weekday } from './todosTemplateHelpers';
 import { addToAllYears } from './allYearsHelpers';
+import { ID, TodoType, TodoTypeValueMap } from './allTodosHelpers';
 
-function updateYearEntry(year, todoData) {
-  localStorage.setItem(year, JSON.stringify(todoData));
+// since the type is stored globally and can change without local values adapting, types and values might disagree
+// though, this only happens when reading from the store; when writing, make sure types and values agree
+export interface LocalTodoData<Type extends TodoType = TodoType> {
+  value: TodoTypeValueMap[Type];
 }
 
-export function returnYearEntry(year) {
-  return JSON.parse(localStorage.getItem(year));
+type Nullish = null | undefined; // helpful if indexes might be undefined or null due to JSON.stringify
+export interface DayTodoData {
+  [id: number]: LocalTodoData;
+}
+type MonthTodoData = (DayTodoData | Nullish)[]; // there are vacant indexes, so that days and indexes match
+type YearTodoData = (MonthTodoData | Nullish)[]; // there are vacant indexes, so that months and indexes match
+type ValidMonthTodoData = DayTodoData[]; // can be used if a specific date (year-month-day) is validated
+type ValidYearTodoData = ValidMonthTodoData[]; // can be used if a specific date (year-month-day) is validated
+
+function updateYearEntry(year: number, todoData: YearTodoData) {
+  localStorage.setItem(String(year), JSON.stringify(todoData));
+}
+
+export function returnYearEntry(year: number): YearTodoData | null {
+  const yearEntry = localStorage.getItem(String(year));
+  return yearEntry === null ? null : JSON.parse(yearEntry);
+}
+function returnValidYearEntry(year: number): ValidYearTodoData {
+  // if year entry, along with month and day, is already validated, prefer this version to avoid nullish return
+  const yearEntry = localStorage.getItem(String(year));
+  if (yearEntry === null) throw new Error('Year entry is not valid');
+  return JSON.parse(yearEntry);
 }
 
 // make sure date exists in the localStorage
-export function validateTodoData(year, month, day, weekday) {
+export function validateTodoData(year: number, month: number, day: number, weekday: Weekday) {
+  // make sure it's not null
   if (!returnYearEntry(year)) {
     updateYearEntry(year, []); // array for months
     addToAllYears(year);
   }
 
-  const yearEntry = returnYearEntry(year);
+  const yearEntry = returnYearEntry(year)!; // the previous logic validates the entry
 
   if (!yearEntry[month]) {
     yearEntry[month] = []; // array for days
@@ -30,38 +54,53 @@ export function validateTodoData(year, month, day, weekday) {
     !Object.keys(yearEntry[month][day]).length // if it's an empty object, try the template, it might be populated
   ) {
     // use the latest one (might return an empty object)
-    yearEntry[month][day] = returnTodosTemplateForWeekday(weekday); // key-value pairs are used
+    yearEntry[month][day] = returnTodosTemplateForWeekday(weekday);
 
     // if any time unit doesn't exist, day will be recreated, if all of them exist, it won't, so it's only updated here
     updateYearEntry(year, yearEntry);
   }
 }
 
-export function returnTodoData(year, month, day) {
-  const yearEntry = returnYearEntry(year);
+export function returnTodoData(year: number, month: number, day: number) {
+  const yearEntry = returnValidYearEntry(year);
   return yearEntry[month][day];
 }
 
-export function addToTodoData(todoId, year, month, day) {
-  const yearEntry = returnYearEntry(year);
-  yearEntry[month][day][todoId] = { value: '' };
+export function addToTodoData(todoId: ID, year: number, month: number, day: number, type: TodoType = 'checkbox') {
+  const yearEntry = returnValidYearEntry(year);
+  switch (type) {
+    case 'number':
+    case 'checkbox':
+      yearEntry[month][day][todoId] = { value: 0 };
+      break;
+    case 'time':
+    case 'text':
+      yearEntry[month][day][todoId] = { value: '' };
+      break;
+  }
   updateYearEntry(year, yearEntry);
 }
 
-export function removeFromTodoData(todoId, year, month, day) {
-  const yearEntry = returnYearEntry(year);
+export function removeFromTodoData(todoId: ID, year: number, month: number, day: number) {
+  const yearEntry = returnValidYearEntry(year);
   delete yearEntry[month][day][todoId];
   updateYearEntry(year, yearEntry);
 }
 
-export function updateTodoValue(todoId, year, month, day, value) {
-  const yearEntry = returnYearEntry(year);
+export function updateTodoValue<Type extends TodoType>(
+  todoId: ID,
+  year: number,
+  month: number,
+  day: number,
+  value: TodoTypeValueMap[Type],
+) {
+  const yearEntry = returnValidYearEntry(year);
   yearEntry[month][day][todoId].value = value;
   updateYearEntry(year, yearEntry);
 }
 
-export function resetTodoData(year, month, day) {
-  const yearEntry = returnYearEntry(year);
+export function resetTodoData(year: number, month: number, day: number) {
+  const yearEntry = returnValidYearEntry(year);
   yearEntry[month][day] = {};
   updateYearEntry(year, yearEntry);
 }
