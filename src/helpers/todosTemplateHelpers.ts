5e2555ac0d97d2396dc938ce00184c8a0230264b
60x20 <120422100+60x20@users.noreply.github.com>
2024-11-30 18:41:11 +0300

    todosTemplateHelpers adopts ts

--- src/helpers/todosTemplateHelpers.ts
+++ src/helpers/todosTemplateHelpers.ts
@@ -1,9 +1,32 @@
 // todosTemplate: todos that used lastly, used as a template for vacant dates
 
+import { ID } from './allTodosHelpers';
+import { DayTodoData, LocalTodoData } from './todoDataHelpers';
+
+export type BooleanAsNum = 0 | 1;
+
+export type Weekday = 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+export type Frequency = [
+  BooleanAsNum,
+  BooleanAsNum,
+  BooleanAsNum,
+  BooleanAsNum,
+  BooleanAsNum,
+  BooleanAsNum,
+  BooleanAsNum,
+];
+type TemplateTodoData = LocalTodoData & { frequency: Frequency };
+interface TodosTemplate extends DayTodoData {
+  [id: number]: TemplateTodoData;
+}
+// TemplateTodoData can be converted into TodoData by deleting the frequency
+type TransferableTodoData = LocalTodoData & { frequency?: Frequency };
+
 // todosTemplate cached to avoid unnecessary parsing
 export let cachedTodosTemplate = returnTodosTemplate();
 
-function updateTodosTemplate(ObjectOfIds) {
+function updateTodosTemplate(ObjectOfIds: TodosTemplate) {
   localStorage.setItem('todos-template', JSON.stringify(ObjectOfIds));
 
   cachedTodosTemplate = ObjectOfIds; // keeping cached version in sync
@@ -15,42 +38,45 @@ export function validateTodosTemplate() {
   }
 }
 
-function returnTodosTemplate() {
-  return JSON.parse(localStorage.getItem('todos-template'));
+function returnTodosTemplate(): TodosTemplate {
+  const todosTemplateEntry = localStorage.getItem('todos-template');
+  if (todosTemplateEntry !== null) return JSON.parse(todosTemplateEntry);
+  throw new Error(`"TodosTemplate" isn't valid`);
 }
 
-export function returnTodosTemplateForWeekday(weekday) {
+export function returnTodosTemplateForWeekday(weekday: Weekday) {
   const localTodosTemplate = returnTodosTemplate();
-  const todosTemplateForWeekday = {};
+  const todosTemplateForWeekday: DayTodoData = {};
   for (const todoId in localTodosTemplate) {
     if (localTodosTemplate[todoId].frequency[weekday]) {
-      delete localTodosTemplate[todoId].frequency; // frequency only stored in todosTemplate entry
-      todosTemplateForWeekday[todoId] = localTodosTemplate[todoId];
+      const transferableTodoData: TransferableTodoData = { ...localTodosTemplate[todoId] };
+      delete transferableTodoData.frequency; // frequency only stored in todosTemplate entry
+      todosTemplateForWeekday[todoId] = transferableTodoData;
     }
   }
   return todosTemplateForWeekday;
 }
 
-const frequencyEveryDay = [1, 1, 1, 1, 1, 1, 1];
-export const frequencyNever = [0, 0, 0, 0, 0, 0, 0];
-export function addToTodosTemplate(id, frequency = frequencyEveryDay) {
+const frequencyEveryDay: Frequency = [1, 1, 1, 1, 1, 1, 1];
+export const frequencyNever: Frequency = [0, 0, 0, 0, 0, 0, 0];
+export function addToTodosTemplate(id: ID, frequency: Frequency = frequencyEveryDay) {
   const localTodosTemplate = returnTodosTemplate();
   localTodosTemplate[id] = { value: '', frequency };
   updateTodosTemplate(localTodosTemplate);
 }
 
-export function updateFrequencyOnTodosTemplate(id, frequency) {
+export function updateFrequencyOnTodosTemplate(id: ID, frequency: Frequency) {
   const localTodosTemplate = returnTodosTemplate();
   localTodosTemplate[id].frequency = frequency;
   updateTodosTemplate(localTodosTemplate);
 }
 
-export function removeFromTodosTemplate(idToRemove) {
+export function removeFromTodosTemplate(idToRemove: ID) {
   const localTodosTemplate = returnTodosTemplate();
   delete localTodosTemplate[idToRemove];
   updateTodosTemplate(localTodosTemplate);
 }
 
-export function isTodoInTodosTemplate(todoId) {
+export function isTodoInTodosTemplate(todoId: ID) {
   return todoId in cachedTodosTemplate;
 }
