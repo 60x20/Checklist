dbda604c933588d28cf040cddf981f8ba9d233e3
60x20 <120422100+60x20@users.noreply.github.com>
2025-01-25 00:15:46 +0300

    satisfy no-non-null-assertion

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (rootElement === null) throw new Error('No root found');
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
