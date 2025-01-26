58b7db2ae18ac35f11eec2315d340e27c905b1ac
60x20 <120422100+60x20@users.noreply.github.com>
2025-01-26 14:52:14 +0300

    section comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+// components
 import App from './App';
 
 const rootElement = document.getElementById('root');
