543f2015b4b1ad711b242ff70d8764d7a979367b
60x20 <120422100+60x20@users.noreply.github.com>
2025-04-07 23:51:52 +0300

    explain why assertion works in useSafeContext

--- src/custom-hooks/useSafeContext.tsx
+++ src/custom-hooks/useSafeContext.tsx
@@ -7,6 +7,9 @@ export default function useSafeContext<ContextValueType>(
   context: React.Context<ContextValueType>,
 ) {
   const contextValue = useContext(context);
-  assertCondition(contextValue !== null, 'context value is null');
+  assertCondition(
+    contextValue !== null,
+    'the provider initializes the context before any consumer can use it',
+  );
   return contextValue;
 }
