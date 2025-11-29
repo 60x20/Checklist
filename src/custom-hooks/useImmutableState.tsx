fb470f8798839c03d76d1a8661f0386fec6ab7d4
60x20 <120422100+60x20@users.noreply.github.com>
2025-11-30 02:56:41 +0300

    add useImmutableState to enforce Readonly states
    useImmutableState unused and commented out since exhaustive-deps fails to recognize set function returned is consistent across renders

---
+++ src/custom-hooks/useImmutableState.tsx
@@ -0,0 +1,6 @@
+// import { useState } from 'react';
+
+// /** useState with readonly return to ensure immutability */
+// export default function useImmutableState<S>(initialState: S | (() => S)) {
+//   return useState<Readonly<S>>(initialState);
+// }
