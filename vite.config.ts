813d772395efec6a73e3adbdc874b5d910e04925
60x20 <120422100+60x20@users.noreply.github.com>
2026-03-25 19:34:16 +0300

    add comment explaining relative base usage

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // since assets are hosted relative to index.html, keep them relative to it
 });
